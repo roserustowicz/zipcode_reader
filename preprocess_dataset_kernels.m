26d5fb0a2ad675fed642fc4a1106cee1602c911b
Rose Rustowicz <rose.rustowicz@gmail.com>
2019-03-17 16:43:09 -0700

    add script for kernel average

---
+++ preprocess_dataset_kernels.m
@@ -0,0 +1,22 @@
+%{ 
+Create average kernels for typed values 0 - 9 digits from the 
+Kaggle "English Typed Alphabets and Numbers Dataset". Each digit 
+has 1,016 examples. 
+%}
+
+for num = 0:9
+    fnames = [dir(fullfile(strcat('numerical_kernels/kernels_', num2str(num)), '*.png'))];
+    
+    count = 0;
+    img = zeros(128, 128);
+    for fname = 1:size(fnames, 1)
+        cur_fname = strcat(strcat('numerical_kernels/kernels_', num2str(num), '/') , fnames(fname).name);
+        cur_img = double(imread(cur_fname));
+        img = img + cur_img;
+        count = count + 1;
+    end
+    
+    avg_img = img / count;
+    figure; imshow(avg_img, [])
+    imwrite(avg_img, strcat('avg_kernel_', num2str(num), '.png'))
+end
